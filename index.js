f39ada52ac76b123ec354b3cae73c017226131be
jasenmichael <jasen@ready-builder.com>
2018-04-01 04:30:55 -0600

    add landing page route

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const path = require("path");
 const morgan = require('morgan')
 
 app.use(morgan('dev'))
 
 app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/index.html'))
+})
+
 
 app.get('/:searchWord', function (req, res) {
   console.log(req.params);
@@ -16,4 +22,4 @@ app.get('/:searchWord', function (req, res) {
   })
 })
 
-app.listen(env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
